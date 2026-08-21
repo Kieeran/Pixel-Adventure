d016764c34df51920cbd7c9fd2a9e3fcab03230c
Quoc Bao <21521856@gm.uit.edu.vn>
2026-08-21 17:36:12 +0700

    explode break pieces

--- Assets/Pixel-Adventure/Scripts/Box/Breaks.cs
+++ Assets/Pixel-Adventure/Scripts/Box/Breaks.cs
@@ -13,8 +13,11 @@ public class Breaks : MonoBehaviour
     [SerializeField] float _maxLifeTime = 2.5f;
     [SerializeField] int _blinkCount = 4;
     [SerializeField] float _blinkInterval = 0.15f;
+    [SerializeField] float explosionForce = 1f;
 
     public event Action OnAllBreaksDisappear;
+
+    Vector2 centerPoint;
 
     void Awake()
     {
@@ -33,6 +36,13 @@ public class Breaks : MonoBehaviour
             breakOriginPosRot.Add((tf.localPosition, tf.localEulerAngles));
             breakRenderer.Add(tf.GetComponentInChildren<SpriteRenderer>());
         }
+
+        centerPoint = GetIntersectionPoint(
+            breakTransform[0].localPosition,
+            breakTransform[3].localPosition,
+            breakTransform[1].localPosition,
+            breakTransform[2].localPosition
+        );
     }
 
     void Reset()
@@ -55,6 +65,8 @@ public class Breaks : MonoBehaviour
         int remaining = breakTransform.Count;
         for (int i = 0; i < breakTransform.Count; i++)
         {
+            Explode(breakTransform[i]);
+
             // Lambda không copy giá trị i — nó giữ reference đến biến i
             // Vòng lặp chạy xong → i = Count
             // Tất cả callback cùng đọc i = Count → sai
@@ -74,6 +86,13 @@ public class Breaks : MonoBehaviour
                 }
             }));
         }
+    }
+
+    void Explode(Transform breakPiece)
+    {
+        Vector2 dir = ((Vector2)breakPiece.localPosition - centerPoint).normalized;
+        breakPiece.GetComponent<Rigidbody2D>().AddForce(dir * explosionForce, ForceMode2D.Impulse);
+        breakPiece.GetComponent<Rigidbody2D>().AddTorque(UnityEngine.Random.Range(-5f, 5f));
     }
 
     IEnumerator LifeCycleRoutine(SpriteRenderer renderer, Action onComplete)
@@ -102,5 +121,23 @@ public class Breaks : MonoBehaviour
         var color = renderer.color;
         color.a = alpha;
         renderer.color = color;
+    }
+
+    Vector2 GetIntersectionPoint(Vector2 A, Vector2 B, Vector2 C, Vector2 D)
+    {
+        // Vector hướng của 2 đoạn thẳng
+        float rx = B.x - A.x;
+        float ry = B.y - A.y;
+        float sx = D.x - C.x;
+        float sy = D.y - C.y;
+
+        // Tích hướng (Cross Product) của 2 vector hướng
+        float denominator = rx * sy - ry * sx;
+
+        // Tính tỉ lệ t trên đoạn AB
+        float t = ((C.x - A.x) * sy - (C.y - A.y) * sx) / denominator;
+
+        // Trả về tọa độ giao điểm
+        return new Vector2(A.x + t * rx, A.y + t * ry);
     }
 }
