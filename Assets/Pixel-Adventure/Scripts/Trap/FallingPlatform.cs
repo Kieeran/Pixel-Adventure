688eb4a91d92906e9efd7330bfd477f59deca4ce
Quoc Bao <21521856@gm.uit.edu.vn>
2026-09-20 20:08:38 +0700

    add simple FallingPlatform (no main features)

---
+++ Assets/Pixel-Adventure/Scripts/Trap/FallingPlatform.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class FallingPlatform : PlacedObject
+{
+    public override void OnSpawn()
+    {
+        base.OnSpawn();
+    }
+
+    public override void OnDespawn()
+    {
+        base.OnDespawn();
+    }
+}
